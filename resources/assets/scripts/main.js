2e50c690b20f6a88730200b6accb932a22bf90f7
Maël Martin <mmartin@atelier-chevara.com>
2022-03-02 11:51:36 +0100

    Fix : Plugin not working on Firefox

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,7 +1,9 @@
-(function($){
-  $(document).ready(function() {
-    if ($('.acf-field-wpeditor').length) {
-      $('.acf-field-wpeditor .acf-input').append($('#postdivrich'));
+(function ($) {
+  $(document).ready(function () {
+    if ($(".acf-field-wpeditor").length) {
+      $("#postdivrich").on("load", function () {
+        $(".acf-field-wpeditor .acf-input").append($("#postdivrich"));
+      });
     }
   });
 })(jQuery);
